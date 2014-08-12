b2005a489b473dd32a6a9c4700c3a26d9960baae
Sebastian Dahlgren <sebastian.dahlgren@gmail.com>
2014-08-12 08:17:57 +0200

    Updated help text formatting

--- aws_ec2_assign_elastic_ip/command_line_options.py
+++ aws_ec2_assign_elastic_ip/command_line_options.py
@@ -2,6 +2,7 @@
 import argparse
 import sys
 from ConfigParser import SafeConfigParser
+from argparse import RawTextHelpFormatter
 
 if sys.platform in ['win32', 'cygwin']:
     import ntpath as ospath
@@ -13,7 +14,8 @@ SETTINGS.read('{0}/settings.conf'.format(
     ospath.dirname(ospath.realpath(__file__))))
 
 PARSER = argparse.ArgumentParser(
-    description='Assign EC2 Elastic IP to the current instance')
+    description='Assign EC2 Elastic IP to the current instance',
+    formatter_class=RawTextHelpFormatter)
 PARSER.add_argument(
     '--version',
     action='count',
@@ -31,11 +33,11 @@ PARSER.add_argument(
 PARSER.add_argument(
     '--valid-ips',
     help=(
-        'A comma separated list of valid Elastic IPs. You can use CIDR '
-        'expressions to select ranges. Valid examples:'
-        '58.0.0.0/8'
-        '123.213.0.0/16,58.0.0.0/8,195.234.023.0'
-        '195.234.234.23,195.234.234.24'))
+        'A comma separated list of valid Elastic IPs.\nYou can use CIDR '
+        'expressions to select ranges.\nValid examples:\n'
+        '- 58.0.0.0/8\n'
+        '- 123.213.0.0/16,58.0.0.0/8,195.234.023.0\n'
+        '- 195.234.234.23,195.234.234.24\n'))
 ARGS = PARSER.parse_args()
 
 if ARGS.version:
